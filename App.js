3624b0704fa4eb4636c6a5c1862b2e4fb43e73e5
Sudheer Vajrapu <sravan.v1993@gmail.com>
2023-12-20 15:40:28 +0530

    Formatted the Code

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-const parent = React.createElement('div', { id: 'parent'}, [
+const parent = React.createElement('div', { id: 'parent' }, [
     React.createElement('div', { id: 'child1'}, 
         [
             React.createElement('h1',
-                { id: 'heading'},
+                { id: 'heading' },
                 "I'm an H1 tag."),
             React.createElement('h2',
-                { id: 'heading'},
+                { id: 'heading' },
                 "I'm an H2 tag.")
         ]
     ),
-    React.createElement('div', { id: 'child2'}, 
+    React.createElement('div', { id: 'child2' }, 
         [
             React.createElement('h1',
-                { id: 'heading'},
+                { id: 'heading' },
                 "I'm an H1 tag."),
             React.createElement('h2',
-                { id: 'heading'},
+                { id: 'heading' },
                 "I'm an H2 tag.")
         ]
     )
